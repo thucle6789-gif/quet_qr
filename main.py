63874d7d59adfcb37663df8a22bc78c990325af1
thucle6789-gif <thucle6789@gmail.com>
2026-06-06 12:33:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def normalize_role(role_str: str) -> str:
     s = s.replace(' ', '')
     return s  # 'sanxuat' hoặc 'nguoixem' hoặc ''
 
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxB9cagxYoxM8kpbLtkFGKoQ6SND4QNqLbPTwFR1fs0bNUH-KNDFSaYtrTxKJ8VadEv8g/exec"
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxsuMYJvguIrr9S5DHGyQ941_Eu1ZtmRqPYWUQ3ZOFoY2mGoOKKjT83A-iLL7KKeJcz5Q/exec"
 VN_TZ = ZoneInfo("Asia/Ho_Chi_Minh")
 
 DANH_SACH_CONG_DOAN = [
